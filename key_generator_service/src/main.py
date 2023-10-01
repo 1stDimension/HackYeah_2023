a69ed2491b244ad78621f96c9897fc1d82fb7917
atlomak <andrzej.tlomak01@gmail.com>
2023-10-01 09:46:49 +0200

    rm stuff from main, added ecdsa

--- key_generator_service/src/main.py
+++ key_generator_service/src/main.py
@@ -1,68 +1,112 @@
 from fastapi import FastAPI, requests
 from fastapi.exceptions import HTTPException
-from pydantic import BaseModel
-from enum import Enum
+
+from data_classes import Key_request, Key_type
+
 import requests
 
 from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.PublicKey import ECC
 
+from generator_config import settings
 
 app = FastAPI()
 
-
-class Key_type(str, Enum):
-    AES = "AES",
-    RSA = "RSA",
-    ECDSA = "ECDSA",
-
-
-class Key_request(BaseModel):
-    key_type: str
-    key_name: str
-    key_size: int
-
-
-class Key(BaseModel):
-    key_data: str
-    key_type: str
-    key_name: str
-    key_size: int
-
-
-KEYSTORE_HEADERS = {}
-
-KEYSTORE_URL = "http://localhost:5000/v1/keys"
-
 KEYS_SIZES_MAX = {
-        "AES": 2048,
-        }
+    "AES": settings.AES_MAX,
+    "RSA": settings.RSA_MAX,
+    "ECDSA": settings.ECDSA_MAX,
+}
+
 KEYS_SIZES_MIN = {
-        "AES": 20000}
+    "AES": settings.AES_MIN,
+    "RSA": settings.RSA_MIN,
+    "ECDSA": settings.ECDSA_MIN,
+}
+
+KEYSTORE_URL = settings.KEYSTORE_URL
+
 
 @app.post("/v1/generate_key")
 async def get_aes_cipher(key_request: Key_request):
     if key_request.key_type == Key_type.AES:
-        return send_aes_code(key_request)
+
+        response_code = send_rsa_key(key_request)
+        return handle_response_code(response_code, key_request)
+
+    elif key_request.key_type == Key_type.RSA:
+
+        response_code = send_rsa_key(key_request)
+        return handle_response_code(response_code, key_request)
+
+    elif key_request.key_type == Key_type.ECDSA:
+
+        response_code = send_ecdsa_key(key_request)
+        return handle_response_code(response_code, key_request)
+
     else:
         raise HTTPException(400, "Wrong key type, allowed: AES, RSA, ECDSA")
 
-def send_aes_code(key_request: Key_request):
+
+def handle_response_code(response_code: int, key_request: Key_request):
+    if response_code == 200:
+        return key_request
+    if response_code == 400:
+        raise HTTPException(
+            400, "Key already exists. New key not added to keystore")
+    raise HTTPException(500, "Internal error. Key not added to keystore")
+
+
+def send_aes_key(key_request: Key_request):
     check_size_guard(key_request.key_size, Key_type.AES)
     key_bytes = get_random_bytes(key_request.key_size)
 
     files = {'data': ("key", key_bytes),
-                                 "name": (None, key_request.key_name),
-                                 "type": (None, key_request.key_type),
-                                 "size": (None, key_request.key_size),}
+             "name": (None, key_request.key_name),
+             "type": (None, key_request.key_type),
+             "size": (None, key_request.key_size), }
 
     response = requests.post(KEYSTORE_URL, files=files)
-    if response.status_code == 201:
-        return key_request
-    else:
-        raise HTTPException(500, "Internal error. Key not addded to keystore")
+    return response.status_code
+
+
+def send_rsa_key(key_request: Key_request):
+    check_size_guard(key_request.key_size, Key_type.AES)
+
+    key = RSA.generate(key_request.key_size)
+    private_key = key.export_key()
+
+    print(private_key)
+
+    files = {'data': ("key", private_key),
+             "name": (None, key_request.key_name),
+             "type": (None, key_request.key_type),
+             "size": (None, key_request.key_size), }
+
+    response = requests.post(KEYSTORE_URL, files=files)
+    return response.status_code
+
+
+def send_ecdsa_key(key_request: Key_request):
+    size = key_request.key_size
+    try:
+        key = ECC.generate(curve=f"P-{size}")
+    except KeyError:
+        raise HTTPException(400, f"ECDSA key size not allowed. Allowed values: 192, 224, 256, 384, 521. Got: {size}")
+
+    private_key = key.export_key(format="PEM")
+
+    files = {'data': ("key", private_key),
+             "name": (None, key_request.key_name),
+             "type": (None, key_request.key_type),
+             "size": (None, key_request.key_size), }
+    response = requests.post(KEYSTORE_URL, files=files)
+    return response.status_code
+
 
 def check_size_guard(size: int, key_type: Key_type):
     if KEYS_SIZES_MAX[key_type] <= size:
-        raise HTTPException(400, "Key size not allowed. Key size too small")
-    if KEYS_SIZES_MIN[key_type] <= size:
         raise HTTPException(400, "Key size not allowed. Key size too big")
+    if KEYS_SIZES_MIN[key_type] >= size:
+        raise HTTPException(400, "Key size not allowed. Key size too small")
--- key_generator_service/src/test/test_endpoint.py
+++ key_generator_service/src/test/test_endpoint.py
@@ -1,5 +1,5 @@
 import pytest
-from main import app
+from main import Key_request, app, send_aes_key, send_rsa_key, send_ecdsa_key
 
 from fastapi.testclient import TestClient
 
@@ -7,12 +7,51 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 AES_PAYLOAD = {
-        "key_type": "AES",
-        "key_size": 16,
-        "key_name": "TestName"
-        }
+    "key_type": "AES",
+    "key_size": 2048,
+    "key_name": "TestAES"
+}
 
-def test_read_item():
+RSA_PAYLOAD = {
+    "key_type": "RSA",
+    "key_size": 2048,
+    "key_name": "TestRSA"
+}
+
+ECDSA_PAYLOAD = {
+    "key_type": "ECDSA",
+    "key_size": 521,
+    "key_name": "TestECDSA"
+}
+
+KEY_REQUEST_AES = Key_request(key_type="AES", key_name="TestAES2", key_size=2048)
+KEY_REQUEST_RSA = Key_request(key_type="RSA", key_name="TestRSA2", key_size=2048)
+KEY_REQUEST_ECDSA = Key_request(key_type="ECDSA", key_name="TestECDSA5", key_size=522)
+
+def test_aes_request():
     response = client.post("/v1/generate_key", json=AES_PAYLOAD)
+    print(response.request.read())
     assert response.status_code == 200
     assert response.json() == AES_PAYLOAD
+
+
+def test_rsa_key():
+    response = client.post("/v1/generate_key", json=RSA_PAYLOAD)
+    print(response.request.read())
+    assert response.status_code == 200
+
+## Integration tests
+
+def test_add_aes_key():
+    response = send_aes_key(KEY_REQUEST_AES)
+    print(response)
+
+
+def test_add_rsa_key():
+    response = send_rsa_key(KEY_REQUEST_RSA)
+    print(response)
+
+
+def test_add_ecdsa_key():
+    response = send_ecdsa_key(KEY_REQUEST_ECDSA)
+    print(response)
